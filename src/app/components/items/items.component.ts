21bed295557a79a3a99ebce27f6bc6baf79e6b7e
SergioCarvajalV <scarvajal@utem.cl>
2022-08-26 19:20:37 -0400

    card creada la cual contiene los items

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-items',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
+
+  items: Item[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      { id: 1, name: 'Item 1', description: '1 description', completed: false },
+      { id: 2, name: 'Item 2', description: '2 description', completed: false },
+    ]
   }
 
 }
