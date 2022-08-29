222b605bb3b3f392ee4b90b1441aeaaa6f2c4a1d
SergioCarvajalV <scarvajal@utem.cl>
2022-08-29 00:37:58 -0400

    se quito la ruta crear y en lugar de eso un boton abrira un componente modal para crear item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddItemComponent } from './components/add-item/add-item.component';
 import { ItemsComponent } from './components/items/items.component';
 
 const routes: Routes = [
@@ -8,10 +7,6 @@ const routes: Routes = [
     path:'',
     component: ItemsComponent,
   },
-  {
-    path:'crear',
-    component: AddItemComponent,
-  }
 ];
 
 @NgModule({
